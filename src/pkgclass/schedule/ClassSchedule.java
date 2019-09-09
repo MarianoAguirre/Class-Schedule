6fcd4322ba77735ed570db3fc1b20f6aa5b40085
21006 <21006@MININT-VTTS8LK.columbushs.com>
2019-09-09 09:24:08 -0400

    Done

--- src/pkgclass/schedule/ClassSchedule.java
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -15,7 +15,34 @@ public class ClassSchedule {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String name = "Mariano";
+        int graduate = 2021;
+        String class1 = "Religion";
+        String class2 = "AP Computer Science";
+        String class3 = "Chemistry";
+        String class4 = "American History";
+        String class5 = "Band";
+        String class6 = "English";
+        String class7 = "Math";
+        String teacher1 = "Mr. Gonzalez";
+        String teacher2 = "Mr. Larrubia";
+        String teacher3 = "Mrs. Reynaldo";
+        String teacher4 = "Mr. Benitez";
+        String teacher5 = "Mr. Cooper";
+        String teacher6 = "Mrs. Pina";
+        String teacher7 = "Mrs. Leonard";
+        
+        
+        System.out.println("+-----------------------------------------------------+");
+        System.out.println("| 1 |    " + class1 + " |  " + teacher1 + "                     | ");
+        System.out.println("| 2 |    " + class2 + " |  " + teacher2 + "          | ");
+        System.out.println("| 3 |    " + class3 + " |  " + teacher3 + "                   | ");
+        System.out.println("| 4 |    " + class4 + " |  " + teacher4 + "              | ");
+        System.out.println("| 5 |    " + class5 + " |  " + teacher5 + "                           | ");
+        System.out.println("| 6 |    " + class6 + " |  " + teacher6 + "                         | ");
+        System.out.println("| 7 |    " + class7 + " |  " + teacher7 + "                         | ");
+        System.out.println("+-----------------------------------------------------+");
+        
     }
     
 }
